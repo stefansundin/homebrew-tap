e1048a481538815e9bf284964afdcda1f8dbe708
Stefan Sundin <git@stefansundin.com>
2017-04-03 21:32:08 -0700

    Add vlc-protocol cask.

---
+++ Casks/vlc-protocol.rb
@@ -0,0 +1,8 @@
+cask 'vlc-protocol' do
+  name 'VLC-protocol'
+  app 'VLC-protocol.app'
+  homepage 'https://github.com/stefansundin/vlc-protocol'
+  version '1.0.0'
+  url "https://github.com/stefansundin/vlc-protocol/releases/download/v#{version}/VLC-protocol.app.zip"
+  sha256 '71a2a70b9f024bb26db13c1c0cc4f361bd42e25ef6c8529133469de45e95a7a0'
+end
---
+++ README.md
@@ -0,0 +1,3 @@
+```
+brew cask install stefansundin/tap/vlc-protocol
+```
