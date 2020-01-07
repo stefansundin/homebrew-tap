839a7e629b2203c620f331cdba6ad107dd899818
Stefan Sundin <git@stefansundin.com>
2020-01-06 18:10:40 -0800

    Update aws-rotate-key.

--- Formula/aws-rotate-key.rb
+++ Formula/aws-rotate-key.rb
@@ -1,8 +1,9 @@
 class AwsRotateKey < Formula
-  desc "Easily rotate your AWS key."
+  desc "Easily rotate your AWS key. 🔑"
   homepage "https://github.com/stefansundin/aws-rotate-key"
-  url "https://github.com/stefansundin/aws-rotate-key/releases/download/v1.0.0/aws-rotate-key-1.0.0-darwin_amd64.zip"
-  sha256 "635882b25a13d893d980eb0b90dcd3623c106ac10ec5926070765686f20f9429"
+  version "1.0.7"
+  url "https://github.com/stefansundin/aws-rotate-key/releases/download/v#{version}/aws-rotate-key-#{version}-darwin_amd64.zip"
+  sha256 "972d52f480c8e6efe1d2b2a95756a5eef6dff595281b17126b39bc768eee3b9d"
 
   def install
     bin.install "aws-rotate-key"
