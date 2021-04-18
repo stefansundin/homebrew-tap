14aebb1da7fc5c3e86b49b061a3b4356484fad26
Stefan Sundin <git@stefansundin.com>
2021-04-17 20:06:03 -0700

    vlc-protocol v1.1.0

--- Casks/vlc-protocol.rb
+++ Casks/vlc-protocol.rb
@@ -2,7 +2,7 @@ cask 'vlc-protocol' do
   name 'VLC-protocol'
   app 'VLC-protocol.app'
   homepage 'https://github.com/stefansundin/vlc-protocol'
-  version '1.0.0'
+  version '1.1.0'
   url "https://github.com/stefansundin/vlc-protocol/releases/download/v#{version}/VLC-protocol.app.zip"
-  sha256 '71a2a70b9f024bb26db13c1c0cc4f361bd42e25ef6c8529133469de45e95a7a0'
+  sha256 'a05e07e100cb322315a724bb6f77139a79d78ae52d03036663d9a7d9d67f71dc'
 end
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ```
+brew tap stefansundin/tap
+
 brew install stefansundin/tap/ssh-tunnel-proxy
 HOMEBREW_NO_SANDBOX=1 brew install --HEAD stefansundin/tap/vlc-twitch
-brew cask install stefansundin/tap/vlc-protocol
+brew install --cask stefansundin/tap/vlc-protocol
 ```
